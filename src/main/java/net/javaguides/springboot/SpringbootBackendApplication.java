19d05265c4c154970cbb81b068a0b4b4597d5383
SAIDAELOUARDI23 <132398432+SAIDAELOUARDI23@users.noreply.github.com>
2024-01-09 01:40:23 +0100

    Update SpringbootBackendApplication.java

--- src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
+++ src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -2,7 +2,6 @@ package net.javaguides.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@ComponentScan(basePackages = "controller", "repository")
 @SpringBootApplication
 public class SpringbootBackendApplication {
 
